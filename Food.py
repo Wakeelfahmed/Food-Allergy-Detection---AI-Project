46da76010ef7d3ef94fed412f7487b9e030b9801
Wakeel <wakeelfahmed@gmail.com>
2024-05-29 00:49:54 +0500

    GUI Deployment

--- Food.py
+++ Food.py
@@ -1,11 +1,6 @@
-import numpy as np
-from helper_function import load_and_prep_image
-# import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
-import csv
 import tensorflow as tf
 import os
-
 import csv
 import pandas as pd
 import numpy as np
@@ -24,35 +19,62 @@ def recognize_allergens(user_input, food_column='Food Product'):
     food_data = df.iloc[most_similar_index]
     return food_data
 
-print(tf.__version__)
 
-class_names = []
-with open("C:\class_names.csv", mode='r', newline='') as file:
-    reader = csv.reader(file)
-    # Skip the header row
-    next(reader)
-    # Iterate over rows and append class names to the list
-    for row in reader:
-        class_names.append(row[1])
-loaded_classes = [
-    'apple pie', 'baby back ribs', 'baklava', 'beef carpaccio', 'beef tartare', 'beet salad', 'beignets', 'bibimbap',
-    'bread pudding', 'breakfast burrito', 'bruschetta', 'caesar salad', 'cannoli', 'caprese salad', 'carrot cake',
-    'ceviche', 'cheesecake', 'cheese plate', 'chicken curry', 'chicken quesadilla', 'chicken wings', 'chocolate cake',
-    'chocolate mousse', 'churros', 'clam chowder', 'club sandwich', 'crab cakes', 'creme brulee', 'croque madame',
-    'cup cakes', 'deviled eggs', 'donuts', 'dumplings', 'edamame', 'eggs benedict', 'escargots', 'falafel',
-    'filet mignon', 'fish and chips', 'foie gras', 'french fries', 'french onion soup', 'french toast',
+print(tf.__version__)  # must be 2.4.1 (python 3.8)
+
+# class_names = []      #use csv or loading or hardcoded below
+
+# with open("C:\class_names.csv", mode='r', newline='') as file:
+#     reader = csv.reader(file)
+#     # Skip the header row
+#     next(reader)
+#     # Iterate over rows and append class names to the list
+#     for row in reader:
+#         class_names.append(row[1])
+class_names = [
+    'apple pie', 'baby back ribs', 'Baklava', 'Beef carpaccio', 'beef tartare', 'beet salad', 'beignets', 'bibimbap',
+    'bread pudding', 'Breakfast burrito', 'bruschetta', 'caesar salad', 'cannoli', 'caprese salad', 'Carrot Cake',
+    'ceviche', 'cheesecake', 'cheese plate', 'Bhicken curry', 'chicken quesadilla', 'Chicken Wings', 'Chocolate Cake',
+    'Chocolate mousse', 'churros', 'Blam chowder', 'club sandwich', 'crab cakes', 'creme brulee', 'croque madame',
+    'Cup cakes', 'Deviled eggs', 'Donuts', 'dumplings', 'edamame', 'eggs benedict', 'escargots', 'falafel',
+    'filet mignon', 'Fish and chips', 'foie gras', 'french fries', 'french onion soup', 'french toast',
     'fried calamari', 'fried rice', 'frozen yogurt', 'garlic bread', 'gnocchi', 'greek salad',
-    'grilled cheese sandwich', 'grilled salmon', 'guacamole', 'gyoza', 'hamburger', 'hot and sour soup', 'hot dog',
-    'huevos rancheros', 'hummus', 'ice cream', 'lasagna', 'lobster bisque', 'lobster roll sandwich',
+    'grilled cheese sandwich', 'Grilled salmon', 'guacamole', 'gyoza', 'hamburger', 'hot and sour soup', 'hot dog',
+    'huevos rancheros', 'hummus', 'Ice Cream', 'lasagna', 'lobster bisque', 'lobster roll sandwich',
     'macaroni and cheese', 'macarons', 'miso soup', 'mussels', 'nachos', 'omelette', 'onion rings', 'oysters',
-    'pad thai', 'paella', 'pancakes', 'panna cotta', 'peking duck', 'pho', 'pizza', 'pork chop', 'poutine', 'prime rib',
-    'pulled pork sandwich', 'ramen', 'ravioli', 'red velvet cake', 'risotto', 'samosa', 'sashimi', 'scallops',
-    'seaweed salad', 'shrimp and grits', 'spaghetti bolognese', 'spaghetti carbonara', 'spring rolls', 'steak',
-    'strawberry shortcake', 'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna tartare', 'waffles'
+    'pad thai', 'paella', 'pancakes', 'Panna cotta', 'peking duck', 'pho', 'pizza', 'pork chop', 'poutine', 'prime rib',
+    'pulled pork sandwich', 'ramen', 'Ravioli', 'red velvet cake', 'risotto', 'Samosa', 'sashimi', 'Scallops',
+    'seaweed salad', 'shrimp and grits', 'Spaghetti bolognese', 'spaghetti carbonara', 'Spring rolls', 'Steak',
+    'strawberry shortcake', 'sushi', 'tacos', 'takoyaki', 'Tiramisu', 'Tuna tartare', 'Waffles'
 ]
 print("Loaded classes: ", class_names)
 
-model = load_model("C:\EfficientNetB1 (1).hdf5")  #org
+model = load_model("EfficientNetB1 (1).hdf5")  #org
+
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+    """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+    # Read in the image
+    img = tf.io.read_file(filename)
+    # Decode it into a tensor
+    img = tf.image.decode_jpeg(img)
+    # Resize the image
+    img = tf.image.resize(img, [img_shape, img_shape])
+    if scale:
+        # Rescale the image (get all values between 0 and 1)
+        return img / 255.
+    else:
+        return img
 
 
 def pred_plot_custom(folder_path):
@@ -61,15 +83,12 @@ def pred_plot_custom(folder_path):
     # fig, a = plt.subplots(len(custom_food_images), 2, figsize=(15, 5 * len(custom_food_images)))
 
     # Load the TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path="C:\Finalmodeltflite_2.4.1_fullepoch.tflite")
+    interpreter = tf.lite.Interpreter(model_path="Finalmodeltflite_2.4.1_fullepoch.tflite")
     interpreter.allocate_tensors()
 
     # Get input and output tensors.
     input_details = interpreter.get_input_details()
-    print(input_details)
-
     output_details = interpreter.get_output_details()
-    print(output_details)
 
     for img in custom_food_images:
         print(img)  # show image path & name
@@ -85,8 +104,6 @@ def pred_plot_custom(folder_path):
         # Plotting Image
         # a[i][0].imshow(img / 255.)
         print(f"Prediction: {pred_class}")
-        food_data = recognize_allergens("Chocolate Cake", food_column='Food Product')
-        print(food_data)
 
         # a[i][0].set_title(f"Prediction: {pred_class}   Probability: {pred_prob.max():.2f}")
         # a[i][0].axis(False)
@@ -109,108 +126,10 @@ def pred_plot_custom(folder_path):
         pred_class1 = class_names[pred_prob1.argmax()]
 
         print(f"tflite Prediction: {pred_class1}")
+        food_data = recognize_allergens(pred_class, food_column='Food Product')
+        print(food_data, "\n")
 
         # i = i + 1
 
 
-pred_plot_custom("C:/Images/")
-
-import tensorflow as tf
-
-
-# model = tf.keras.models.load_model("C:\EfficientNetB1 (1).hdf5")
-# print("Model loaded successfully.")
-# print("Coverting.")
-#
-# converter = tf.lite.TFLiteConverter.from_keras_model(model)
-# converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
-# tflite_model = converter.convert()
-#
-# # Save the converted model to a .tflite file
-# with open("efficientnetb1.tflite", "wb") as f:
-#     f.write(tflite_model)
-
-# Define the function to make predictions and plot the results
-def pred_plot_custom_tflite(folder_path):
-    # Load the TFLite model and allocate tensors
-    tflite_model_path = "C:\\amodeltflite.tflite"
-    interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
-    interpreter.allocate_tensors()
-
-    # Get input and output tensors
-    input_details = interpreter.get_input_details()
-    print("Input")
-    print(input_details)
-    print("OUTPUT")
-
-    output_details = interpreter.get_output_details()
-    print(output_details)
-
-# pred_plot_custom_tflite("C:\Images\\")
-
-# import os
-# import csv
-# import matplotlib.pyplot as plt
-# import tensorflow as tf
-#
-#
-# # Function to load and preprocess image
-# def load_and_prep_image(img_path, scale=False):
-#     img = tf.io.read_file(img_path)
-#     img = tf.image.decode_image(img, channels=3)
-#     img = tf.image.resize(img, [224, 224])
-#     if scale:
-#         img /= 255.0  # normalize to [0,1] range
-#     return img
-#
-#
-# # Load class names from CSV file
-# def load_class_names(csv_file_path):
-#     class_names = []
-#     with open(csv_file_path, mode='r', newline='') as file:
-#         reader = csv.reader(file)
-#         next(reader)  # Skip the header row
-#         for row in reader:
-#             class_names.append(row[1])
-#     return class_names
-#
-#
-# # Function to predict and plot images
-# def pred_plot_custom(folder_path):
-#   import os
-#
-#   custom_food_images = [folder_path + img_path for img_path in os.listdir(folder_path)]
-#   i=0
-#   fig,a =  plt.subplots(len(custom_food_images),2, figsize=(15, 5*len(custom_food_images)))
-#
-#   for img in custom_food_images:
-#     img = load_and_prep_image(img, scale=False)
-#     pred_prob = loaded_model.predict(tf.expand_dims(img, axis=0))
-#     pred_class = class_names[pred_prob.argmax()]
-#     top_5_i = (pred_prob.argsort())[0][-5:][::-1]
-#     values = pred_prob[0][top_5_i]
-#     labels = []
-#     for x in range(5):
-#       labels.append(class_names[top_5_i[x]])
-#
-#     # Plotting Image
-#     a[i][0].imshow(img/255.)
-#     a[i][0].set_title(f"Prediction: {pred_class}   Probability: {pred_prob.max():.2f}")
-#     a[i][0].axis(False)
-#
-#     # Plotting Models Top 5 Predictions
-#     a[i][1].bar(labels, values, color='orange');
-#     a[i][1].set_title('Top 5 Predictions')
-#     i=i+1
-# # Define paths
-#
-# csv_file_path = r"C:\class_names.csv"
-# #"C:\class_names.csv"
-# #"C:\FoodVisiontest.keras"
-# #"C:\FinalModel.hdf5"
-# #"C:\EfficientNetB1 (1).hdf5"
-# model_path = r"C:\FinalModel.hdf5"
-# image_folder_path = r"C:\Images"
-#
-# # Load class names
-# class_names = load_class_names(csv_file_path)
+pred_plot_custom("C:/Images/")  # Run the model and make allergen predictions on all the images in the folder
---
+++ GUI.py
@@ -0,0 +1,122 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import os
+
+import csv
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv("food_ingredients_and_allergens.csv")
+
+def recognize_allergens(user_input, food_column='Food Product'):
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer.fit(df[food_column])
+    user_input_tfidf = tfidf_vectorizer.transform([user_input])
+    cosine_similarities = cosine_similarity(user_input_tfidf, tfidf_vectorizer.transform(df[food_column]))
+    most_similar_index = np.argmax(cosine_similarities)
+    food_data = df.iloc[most_similar_index]
+    return food_data
+
+class_names = [
+    'apple pie', 'baby back ribs', 'Baklava', 'Beef carpaccio', 'beef tartare', 'beet salad', 'beignets', 'bibimbap',
+    'bread pudding', 'Breakfast burrito', 'bruschetta', 'caesar salad', 'cannoli', 'caprese salad', 'Carrot Cake',
+    'ceviche', 'cheesecake', 'cheese plate', 'Bhicken curry', 'chicken quesadilla', 'Chicken Wings', 'Chocolate Cake',
+    'Chocolate mousse', 'churros', 'Blam chowder', 'club sandwich', 'crab cakes', 'creme brulee', 'croque madame',
+    'Cup cakes', 'Deviled eggs', 'Donuts', 'dumplings', 'edamame', 'eggs benedict', 'escargots', 'falafel',
+    'filet mignon', 'Fish and chips', 'foie gras', 'french fries', 'french onion soup', 'french toast',
+    'fried calamari', 'fried rice', 'frozen yogurt', 'garlic bread', 'gnocchi', 'greek salad',
+    'grilled cheese sandwich', 'Grilled salmon', 'guacamole', 'gyoza', 'hamburger', 'hot and sour soup', 'hot dog',
+    'huevos rancheros', 'hummus', 'Ice Cream', 'lasagna', 'lobster bisque', 'lobster roll sandwich',
+    'macaroni and cheese', 'macarons', 'miso soup', 'mussels', 'nachos', 'omelette', 'onion rings', 'oysters',
+    'pad thai', 'paella', 'pancakes', 'Panna cotta', 'peking duck', 'pho', 'pizza', 'pork chop', 'poutine', 'prime rib',
+    'pulled pork sandwich', 'ramen', 'Ravioli', 'red velvet cake', 'risotto', 'Samosa', 'sashimi', 'Scallops',
+    'seaweed salad', 'shrimp and grits', 'Spaghetti bolognese', 'spaghetti carbonara', 'Spring rolls', 'Steak',
+    'strawberry shortcake', 'sushi', 'tacos', 'takoyaki', 'Tiramisu', 'Tuna tartare', 'Waffles'
+]
+
+print("Loaded classes: ", class_names)
+
+model = load_model("EfficientNetB1 (1).hdf5")  #org
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+    img = tf.io.read_file(filename)
+    img = tf.image.decode_jpeg(img)
+    img = tf.image.resize(img, [img_shape, img_shape])
+    if scale:
+        return img / 255.
+    else:
+        return img
+
+def pred_plot_custom(folder_path):
+    interpreter = tf.lite.Interpreter(model_path="Finalmodeltflite_2.4.1_fullepoch.tflite")
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    img = load_and_prep_image(folder_path, scale=False)
+    pred_prob = model.predict(tf.expand_dims(img, axis=0))
+    pred_class = class_names[pred_prob.argmax()]
+
+    img1 = np.expand_dims(img, axis=0).astype(np.float32)
+    interpreter.set_tensor(input_details[0]['index'], img1)
+    interpreter.invoke()
+    pred_prob1 = interpreter.get_tensor(output_details[0]['index'])
+    pred_class1 = class_names[pred_prob1.argmax()]
+
+    food_data = recognize_allergens(pred_class, food_column='Food Product')
+
+    # Display predictions in the GUI using Treeview
+    tree.delete(*tree.get_children())
+    tree.insert('', 'end', values=('Prediction', pred_class))
+    tree.insert('', 'end', values=('TFLite Prediction', pred_class1))
+    tree.insert('', 'end', values=('Food Product', food_data['Food Product']))
+    tree.insert('', 'end', values=('Main Ingredient', food_data['Main Ingredient']))
+    tree.insert('', 'end', values=('Sweetener', food_data['Sweetener']))
+    tree.insert('', 'end', values=('Fat/Oil', food_data['Fat/Oil']))
+    tree.insert('', 'end', values=('Seasoning', food_data['Seasoning']))
+    tree.insert('', 'end', values=('Allergens', food_data['Allergens']))
+    tree.insert('', 'end', values=('Prediction', 'Contains' if food_data['Allergens'] else 'Does not contain'))
+
+def select_image():
+    file_path = filedialog.askopenfilename(
+        filetypes=[("Image files", "*.jpg;*.jpeg;*.png;*.gif;*.bmp")]
+    )
+    if not file_path:
+        return
+
+    image = Image.open(file_path)
+    image = image.resize((400, 400))
+    image_tk = ImageTk.PhotoImage(image)
+
+    image_label.config(image=image_tk)
+    image_label.image = image_tk
+
+    pred_plot_custom(file_path)
+
+root = tk.Tk()
+root.title("Food Detection and Allergy Prediction")
+
+# Create a label to display the selected image
+image_label = tk.Label(root)
+image_label.pack(padx=10, pady=10)
+
+# Create a button to open the file dialog
+select_button = tk.Button(root, text="Select Image", command=select_image)
+select_button.pack(pady=10)
+
+# Create a Treeview widget to display predictions
+tree = ttk.Treeview(root, columns=('Attribute', 'Value'), show='headings')
+tree.heading('Attribute', text='Attribute')
+tree.heading('Value', text='Value')
+tree.pack(pady=10)
+
+# Run the application
+root.mainloop()
